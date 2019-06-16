14493133f011228ad564dc78c963a5421a26b5c8
Gabriel <g2p.code@gmail.com>
2019-06-16 12:39:22 +0200

    Tweak tuning parameters, add a flush fudge factor

--- trace_test_wodan.ml
+++ trace_test_wodan.ml
@@ -47,8 +47,10 @@ end
 module Stor = Wodan.Make(Block)(struct
   include Wodan.StandardSuperblockParams
   let key_size = 64
-  let block_size = 64*1024
+  let block_size = 32*1024
 end)
+
+let flush_period = 1024
 
 let v () =
   let root = "wodan.img" in
@@ -57,7 +59,7 @@ let v () =
   let%lwt () = Nocrypto_entropy_lwt.initialize () in
   let%lwt (stor, _gen) = Stor.prepare_io
     (Wodan.FormatEmptyDevice Int64.(div (mul info.size_sectors @@ of_int info.sector_size) @@ of_int Stor.P.block_size)) disk
-    {Wodan.standard_mount_options with cache_size=24*1024; relax = { magic_crc = true; magic_crc_write = true}}
+    {Wodan.standard_mount_options with cache_size=2*64*1024; relax = { magic_crc = true; magic_crc_write = true}}
   in Lwt.return stor
 
 let f = open_in_bin path
@@ -66,12 +68,12 @@ let t1 = Unix.gettimeofday ()
 
 let%lwt () =
   Sys.catch_break true;
-  (*Logs.set_reporter @@ Logs.format_reporter ();
-  Logs.set_level @@ Some Logs.Debug;*)
+  Logs.set_reporter @@ Logs.format_reporter ();
+  Logs.set_level @@ Some Logs.Info;
   let%lwt stor = v () in
-  let t = Unix.gettimeofday () in
   let mkl = ref 0 in
-  let rec loop t =
+  let flush_req_counter = ref 0 in
+  let rec loop () =
     let elt = read_one f in
     match elt with
     | Read k ->
@@ -82,26 +84,28 @@ let%lwt () =
          | Some _b -> ()
          | None -> failwith (Printf.sprintf "Missing key %s" k)
         );
-        loop t
+        loop ()
     | Mem k ->
         let mkl' = String.length k in
         if mkl' > !mkl then mkl := mkl';
         let%lwt b = Stor.mem stor @@ Stor.key_of_string_padded k in
         assert b;
-        loop t
+        loop ()
     | Write (k, l) ->
         let mkl' = String.length k in
         if mkl' > !mkl then mkl := mkl';
         let%lwt () = Stor.insert stor (Stor.key_of_string_padded k) (Stor.value_of_string @@ String.make l '\000') in
-        loop t
+        loop ()
     | Commit ->
-        let%lwt _gen = Stor.flush stor in
-        let t' = Unix.gettimeofday () in
-        (*Printf.printf "%0.2f\n%!" (t' -. t);*)
-        loop t'
+        flush_req_counter := succ !flush_req_counter;
+        if !flush_req_counter mod flush_period = 0 then
+          let%lwt _gen = Stor.flush stor in
+          loop ()
+        else
+          loop ()
   in
   try
-    loop t
+    loop ()
   with
   |End_of_file ->
       Printf.printf "Max key length %d\n" !mkl;
